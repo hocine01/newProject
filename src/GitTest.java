845832fae4634839a8cf47d3ac3a7f2da7319582
hocine01 <80495594+hocine01@users.noreply.github.com>
2021-03-16 19:04:57 -0500

    change name

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("100_000_00");
         System.out.println("100");
+        System.out.println("hocine");
     }
 }
